0c18b1ca5c998790f44255ad150f188e278bd75b
goldy-79 <xyryllbutulan@gmail.com>
2026-01-22 22:29:30 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const overlay = document.getElementById("overlay")
+const paper = document.getElementById("paper")
+const lines = document.querySelectorAll(".line")
+const music = document.getElementById("music")
+
+overlay.onclick = () => {
+overlay.style.display = "none"
+paper.style.display = "block"
+music.volume = 0.3
+music.play()
+
+let delay = 0
+lines.forEach(line => {
+setTimeout(() => {
+line.style.opacity = 1
+}, delay)
+delay += 1200
+})
+}
